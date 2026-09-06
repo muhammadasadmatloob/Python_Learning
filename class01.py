36775e76e556c04784e4e69602fd29313b000f4e
Muhammad Asad Matloob <muhammadasadmatloob214@gmail.com>
2026-09-06 18:40:40 +0500

    mini project done

--- class01.py
+++ class01.py
@@ -50,3 +50,31 @@ register_worker = cashier("Muhammad Asad Matloob", "Cashier")
 register_worker.clock_in()
 register_worker.scan_item("Headphones")
 
+
+# project
+
+database = []
+incoming_request={
+        "product_name": "iPhone 15",
+        "price": -5000
+    }
+
+valid_admin_token = "ey12345admin"
+
+def delete_product(product_id, provided_token):
+    if provided_token == valid_admin_token:
+        print(f"Status 200: Product with ID {product_id} deleted from database")
+    else:
+        print("Status 403: Error! Invalid admin token.")
+
+delete_product(99, "ey12345admin")
+
+
+def process_order(order_data):
+    if order_data["price"] > 0:
+        print(f"Status 200: Order saved to database")
+        database.append(order_data)
+    else:
+        print("Status 400: Error! Price cannot be negative.")
+
+process_order(incoming_request)
