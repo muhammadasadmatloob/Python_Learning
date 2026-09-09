0e3ff30cf7d4750058efaaa840230ec2bbde6b3a
Muhammad Asad Matloob <muhammadasadmatloob214@gmail.com>
2026-09-09 09:13:40 +0500

    class01 finished

--- class01.py
+++ class01.py
@@ -1,80 +1,105 @@
-# array
-daily_sales=[10000,15000,9000,7000,2000]
-highest_sale = 0 
+# first program
+print("hello world")
 
-for sale in daily_sales:
-    if sale > highest_sale:
-        highest_sale = sale
-    else:
-        pass
+# print() is an fuction - a reusable block of code that dose something.
 
-print("Highest sale:", highest_sale)
+# variables - storing data
+# a variable is anamed box that holds a value
 
+name = "John"
+age = 20
+height = 5.9
 
-# dictionary
+# so the name holds the text called a string(str)
+# age holds a whole number called an integer(int)
+# height holds a decimal number called a float(float)
 
-inventory =[
-    {"name": "headphones", "price": 10000},
-    {"name": "keyboard", "price": 9000},
-    {"name": "mouse", "price": 15000},
+# we dont need to declare the type of variable in python
+# as any other language it automatically detects called dynamic type casting
 
-]
-highest_price = 10000
-for items in inventory:
-    if items["price"] > highest_price:
-        print(f"highest price item name is: {items['name']} and its price is {items['price']} ")
-    else:
-        pass
+print(name)
+print(age)
+print(height)
 
+# data types:
 
-# class
+x = "hello" #string
+y = 25 #integer
+z = 3.14 #float
+a = True #boolean
 
-class employee:
-
-    def __init__(self,name,role):
-        self.name=name
-        self.role=role
-
-    def clock_in(self):
-        print(f"{self.name} clocked in as {self.role}")
-
-class cashier(employee):
-    
-    def scan_item(self,item_name): 
-        print(f"{self.name} just scanned a {item_name}.")
+# to check the type of variable
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
 
 
+# how to take an input from user
+name = input ("enter your name: ")
+print("hello, "+name)
 
-register_worker = cashier("Muhammad Asad Matloob", "Cashier")
+# input by default takes an string even if you type a number
+age = input("enter your age")
+print(type(age))
 
-register_worker.clock_in()
-register_worker.scan_item("Headphones")
+# to use it as a number we have to convert it 
+age = int (input( "Enter your age:" ))
+print(age+3)
+
+# operators
+# there are two type of operators arithmatic and comparison
+# lets see first arithmatic
+print(10+3) #addition
+print(10-3) #subtraction
+print(10*3) #multiplication
+print(10/3) #division
+print(10//3) #floor division
+print(10%3) #modulus
+print(10**3) #power
+
+# comparison operator only checks true and false
+print(10>3) 
+print(10 == 3)
+print(10 != 3 )
+
+# why the % modulus matters
+# this one operator alone solves ton of logic problems :
+# check even/odd , checking divisibility , cycling through positions
+# remeber it you'll use it constantly in dsa
+
+# practice problem
+# take an input veriable for your name and age and print them in an sentence
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"hello my name is {name} and iam {age} years old")
+
+# take two numbers as an input from the user and print there sum
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+print(number1+number2)
+
+# take input number and check its even or odd
+number = int(input("Enter number: "))
+if number%2 == 0:
+    print("even")
+else:
+    print("odd")
 
 
-# project
+# logic building 
 
-database = []
-incoming_request={
-        "product_name": "iPhone 15",
-        "price": -5000
-    }
+# how many years left to turn 100
+# simple and easy
+# first we will take an input age
+# input age - 100 
+# and the remaining years will be those years
+# output will be years left
 
-valid_admin_token = "ey12345admin"
-
-def delete_product(product_id, provided_token):
-    if provided_token == valid_admin_token:
-        print(f"Status 200: Product with ID {product_id} deleted from database")
-    else:
-        print("Status 403: Error! Invalid admin token.")
-
-delete_product(99, "ey12345admin")
-
-
-def process_order(order_data):
-    if order_data["price"] > 0:
-        print(f"Status 200: Order saved to database")
-        database.append(order_data)
-    else:
-        print("Status 400: Error! Price cannot be negative.")
-
-process_order(incoming_request)
+age = int ( input("Enter your age: "))
+years = 100
+years_remains_to_convert_to_100 =  years - age
+print(years_remains_to_convert_to_100)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,4 @@
-inventory =[
-    {"name": "headphones", "price": 10000},
-    {"name": "keyboard", "price": 9000},
-    {"name": "mouse", "price": 15000},
-
-]
-highest_price = 10000
-for items in inventory:
-    if items["price"] > highest_price:
-        print(f"highest price item name is: {items['name']} and its price is {items['price']} ")
-    else:
-        pass
+age = int ( input("Enter your age: "))
+years = 100
+years_remains_to_convert_to_100 =  years - age
+print(years_remains_to_convert_to_100)
